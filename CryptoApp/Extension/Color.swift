26c84cc5fa4a9649bf55edc2d0624540b1776a19
Dostan <turlybekdostan@gmail.com>
2025-04-18 22:16:43 +0500

    0.0.0.1 custom color theme

---
+++ CryptoApp/Extension/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Dostan Turlybek on 18.04.2025.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let backgrounfColor = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
